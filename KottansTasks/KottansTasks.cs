a252ac45417a9e2250449164ee01f162011a0286
waterdev <markyantonyak@gmail.com>
2016-05-03 00:40:18 +0300

    Last commit with test provided

---
+++ KottansTasks/KottansTasks.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace KottansTasks
+{
+    public static class KottansTasks
+    {
+        public static string GetCreditCardVendor(string cardNumber)
+        {
+            //Remove all whitespaces
+            cardNumber = new string(cardNumber.Where(c => !char.IsWhiteSpace(c)).ToArray());
+
+            //Create Regex check patterns
+            Regex visaRgx = new Regex("^4[0-9]{6,}$");
+            Regex masterCardRgx = new Regex("^5[1-5][0-9]{5,}$");
+            Regex jcbRgx = new Regex("^(?:2131|1800|35[0-9]{3})[0-9]{3,}$");
+            Regex americanExpressRgx = new Regex("^3[47][0-9]{5,}$");
+            Regex maestroRgx = new Regex(@"(?:5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)[0-9]{8,15}");
+
+            //Check all patterns
+            if (visaRgx.IsMatch(cardNumber))
+                return "VISA";
+            else if (masterCardRgx.IsMatch(cardNumber))
+                return "MasterCard";
+            else if (jcbRgx.IsMatch(cardNumber))
+                return "JCB";
+            else if (americanExpressRgx.IsMatch(cardNumber))
+                return "AMERICAN EXPRESS";
+            else if (maestroRgx.IsMatch(cardNumber))
+                return "Maestro";
+
+
+            return "Unknown";
+        }
+
+
+
+        public static bool IsCreditCardNumberValid(string cardNumber)
+        {
+            //check if all symbols are numbers
+            cardNumber = new string(cardNumber.Where(x => Char.IsNumber(x)).Reverse().ToArray());
+
+            //for every not even index  multiply symbol by 2
+            int sumOfDigits = cardNumber
+                    .Select((e, i) => ((int)e - '0') * (i % 2 == 0 ? 1 : 2)) //e-48 or -'0' is a cast to int 
+                    .Sum((e) => e / 10 + e % 10);
+
+            return sumOfDigits % 10 == 0;        
+        }
+
+        public static int GenerateNextCreditCardNumber(string cardNumber)
+        {
+            //check if all symbols are numbers
+            cardNumber = new string(cardNumber.Where(x => Char.IsNumber(x)).Reverse().ToArray());
+
+            //for every even index  multiply symbol by 2
+            int sumOfDigits = cardNumber
+                    .Select((e, i) => ((int)e - '0') * (i % 2 != 0 ? 1 : 2)) //e-48 or -'0' is a cast to int 
+                    .Sum((e) => e / 10 + e % 10);
+
+            //get the next number of checksum
+            return (10 - (sumOfDigits % 10));
+        }
+    }
+}
---
+++ KottansTasks/TasksTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CreditCardTests
+{
+    [TestClass]
+    public class TasksTests
+    {
+        [TestMethod]
+        public void GetCreditVendorTest()
+        {
+            Assert.AreEqual("VISA", KottansTasks.KottansTasks.GetCreditCardVendor("4916 3385 0608 2832"));
+            Assert.AreEqual("VISA", KottansTasks.KottansTasks.GetCreditCardVendor("4539 048040151731"));
+            Assert.AreEqual("JCB", KottansTasks.KottansTasks.GetCreditCardVendor("3528 000000000000"));
+            Assert.AreEqual("JCB", KottansTasks.KottansTasks.GetCreditCardVendor("3529 000000000000"));
+            Assert.AreEqual("JCB", KottansTasks.KottansTasks.GetCreditCardVendor("3589 000000000000"));
+            Assert.AreEqual("MasterCard", KottansTasks.KottansTasks.GetCreditCardVendor("5442 1796 1969 0834"));
+            Assert.AreEqual("MasterCard", KottansTasks.KottansTasks.GetCreditCardVendor("5259474113320034"));
+            Assert.AreEqual("MasterCard", KottansTasks.KottansTasks.GetCreditCardVendor("5331113404316994"));
+            Assert.AreEqual("Maestro", KottansTasks.KottansTasks.GetCreditCardVendor("5893000000000000"));
+            Assert.AreEqual("Maestro", KottansTasks.KottansTasks.GetCreditCardVendor("6763000000000000"));
+            Assert.AreEqual("Maestro", KottansTasks.KottansTasks.GetCreditCardVendor("5020000000000000"));
+            Assert.AreEqual("AMERICAN EXPRESS", KottansTasks.KottansTasks.GetCreditCardVendor("345936346788903"));
+            Assert.AreEqual("AMERICAN EXPRESS", KottansTasks.KottansTasks.GetCreditCardVendor("371095063560404"));
+        }
+
+        [TestMethod]
+        public void IsCardNumberValid()
+        {
+            Assert.AreEqual(true, KottansTasks.KottansTasks.IsCreditCardNumberValid("4532 2271 9926 9513"));
+            Assert.AreEqual(true, KottansTasks.KottansTasks.IsCreditCardNumberValid("5540 8724 4673 5415"));
+            Assert.AreEqual(true, KottansTasks.KottansTasks.IsCreditCardNumberValid("6011976648160021"));
+            Assert.AreEqual(true, KottansTasks.KottansTasks.IsCreditCardNumberValid("373471048530507"));
+            Assert.AreEqual(true,KottansTasks.KottansTasks.IsCreditCardNumberValid("5168 7572 0868 6966"));
+            Assert.AreEqual(false, KottansTasks.KottansTasks.IsCreditCardNumberValid("516833540 7572 0868 6966"));
+            Assert.AreEqual(false, KottansTasks.KottansTasks.IsCreditCardNumberValid("5168500ssssas0 7592 0868 6966"));
+
+
+        }
+
+        [TestMethod]
+        public void GenerateNextNumber()
+        {
+            Assert.AreEqual(3, KottansTasks.KottansTasks.GenerateNextCreditCardNumber("4532 2271 9926 951"));
+            Assert.AreEqual(5, KottansTasks.KottansTasks.GenerateNextCreditCardNumber("554087244673541"));
+            Assert.AreEqual(1,KottansTasks.KottansTasks.GenerateNextCreditCardNumber("601197664816002"));
+            Assert.AreEqual(7,KottansTasks.KottansTasks.GenerateNextCreditCardNumber("37347104853050"));
+            Assert.AreEqual(6, KottansTasks.KottansTasks.GenerateNextCreditCardNumber("5168 7572 0868 696"));
+
+        }
+    }
+}
